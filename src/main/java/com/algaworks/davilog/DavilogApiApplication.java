d53c4d135617dfd63fa5e175d2a5c3c3bf1df83d
DaviFarias7 <davic.farias7@gmail.com>
2022-01-13 00:44:06 -0300

    first commit

---
+++ src/main/java/com/algaworks/davilog/DavilogApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.davilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavilogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavilogApiApplication.class, args);
+	}
+
+}
